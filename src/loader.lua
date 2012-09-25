6f098d5a5d8f57425f24c8381408884fc232d46d
Philip Horger <campadrenalin@gmail.com>
2012-09-24 22:24:01 -0700

    Experimental changes to loader to facilitate on-the-fly level reloads

--- src/loader.lua
+++ src/loader.lua
@@ -8,163 +8,59 @@ local home = require 'menu'
 local nextState = 'home'
 local nextPlayer = nil
 
+local import_levels = {
+    ['valley']      = 'valley',
+    ['gay-island']  = 'gay-island',
+    ['gay-island2'] = 'gay-island2',
+    ['abedtown']    = 'newtown',
+    ['lab']         = 'lab',
+    ['house']       = 'house',
+    ['studyroom']   = 'studyroom',
+    ['forest']      = 'forest',
+    ['forest2']     = 'forest2',
+    ['black-caverns']  = 'black-caverns',
+    ['village-forest'] = 'village-forest',
+    ['town']        = 'town',
+    ['tavern']      = 'tavern',
+    ['blacksmith']  = 'blacksmith',
+    ['deans-office-1']   = 'deans-office-1',
+    ['deans-office-2']   = 'deans-office-2',
+    ['deans-closet']     = 'deans-closet',
+    ['baseball']    = 'baseball',
+    ['dorm-lobby']  = 'dorm-lobby',
+    ['borchert-hallway'] = 'borchert-hallway',
+    ['admin-hallway']    = 'admin-hallway',
+    ['class-hallway-1']  = 'class-hallway-1',
+    ['class-hallway-2']  = 'class-hallway-2',
+    ['rave-hallway']     = 'rave-hallway',
+    ['class-basement']   = 'class-basement',
+    ['gazette-office-1'] = 'gazette-office-1',
+    ['gazette-office-2'] = 'gazette-office-2',
+}
+local import_interfaces = {
+    ['overworld']   = 'overworld',
+    ['credits']     = 'credits',
+    ['select']      = 'select',
+    ['home']        = 'menu',
+    ['pause']       = 'pause',
+    ['cheatscreen'] = 'cheatscreen',
+    ['instructions']= 'instructions',
+    ['options']     = 'options',
+    ['blackjackgame']    = 'blackjackgame',
+}
+
 function state:init()
     state.finished = false
     state.current = 1
     state.assets = {}
 
-    table.insert(state.assets, function()
-        Gamestate.load('valley', Level.new('valley'))
-    end)
+    for k, v in pairs(import_levels) do
+        table.insert(state.assets, {'level', k})
+    end
 
-    table.insert(state.assets, function()
-        Gamestate.load('gay-island', Level.new('gay-island'))
-    end)
-
-     table.insert(state.assets, function()
-        Gamestate.load('gay-island2', Level.new('gay-island2'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('abedtown', Level.new('newtown'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('lab', Level.new('lab'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('house', Level.new('house'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('studyroom', Level.new('studyroom'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('hallway', Level.new('hallway'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('forest', Level.new('forest'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('forest2', Level.new('forest2'))
-    end)
-
-
-    table.insert(state.assets, function()
-        Gamestate.load('black-caverns', Level.new('black-caverns'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('village-forest', Level.new('village-forest'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('town', Level.new('town'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('tavern', Level.new('tavern'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('blacksmith', Level.new('blacksmith'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('greendale-exterior', Level.new('greendale-exterior'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('deans-office-1', Level.new('deans-office-1'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('deans-office-2', Level.new('deans-office-2'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('deans-closet', Level.new('deans-closet'))
-    end)
-    
-    table.insert(state.assets, function()
-        Gamestate.load('baseball', Level.new('baseball'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('dorm-lobby', Level.new('dorm-lobby'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('borchert-hallway', Level.new('borchert-hallway'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('admin-hallway', Level.new('admin-hallway'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('class-hallway-1', Level.new('class-hallway-1'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('class-hallway-2', Level.new('class-hallway-2'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('rave-hallway', Level.new('rave-hallway'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('class-basement', Level.new('class-basement'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('gazette-office-1', Level.new('gazette-office-1'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('gazette-office-2', Level.new('gazette-office-2'))
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('overworld', require 'overworld')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('credits', require 'credits')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('select', require 'select')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('home', require 'menu')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('pause', require 'pause')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('cheatscreen', require 'cheatscreen')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('instructions', require 'instructions')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('options', require 'options')
-    end)
-
-    table.insert(state.assets, function()
-        Gamestate.load('blackjackgame', require 'blackjackgame')
-    end)
+    for k, v in pairs(import_interfaces) do
+        table.insert(state.assets, {'interface', k})
+    end
 
     state.step = 240 / # self.assets
 end
@@ -177,12 +73,25 @@ function state:update(dt)
     local asset = state.assets[self.current]
 
     if asset ~= nil then
-        asset()
+        self:loadAsset(asset)
         self.current = self.current + 1
     else
         self.finished = true
         self:switch()
     end
+end
+
+function state:loadAsset(asset_data)
+    local atype = asset_data[1]
+    local aname = asset_data[2]
+    local avalue = nil
+    print(aname)
+    if atype == "level" then
+        avalue = Level.new(import_levels[aname])
+    else
+        avalue = require(import_interfaces[aname])
+    end
+    Gamestate.load(aname, avalue)
 end
 
 function state:switch()
